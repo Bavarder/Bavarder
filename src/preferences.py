211ae80e9332c580ac1c92213f8174cc04a80356
0xMRTT <0xMRTT@proton.me>
2023-04-27 00:32:36 +0200

    pref: add preferences for clear after send

---
+++ src/preferences.py
@@ -0,0 +1,31 @@
+from gi.repository import Gtk, Adw
+
+
+@Gtk.Template(resource_path="/com/github/Bavarder/Bavarder/ui/preferences.ui")
+class Preferences(Adw.PreferencesWindow):
+    __gtype_name__ = "GradiencePreferencesWindow"
+
+    clear_after_send_switch = Gtk.Template.Child()
+
+    def __init__(self, application, **kwargs):
+        super().__init__(**kwargs)
+
+        self.app = application
+        self.settings = application.settings
+
+        self.win = self.app.get_active_window()
+
+
+        self.setup()
+
+    def setup(self):
+        clear_after_send = self.settings.get_boolean("clear-after-send")
+        self.clear_after_send_switch.connect("state-set", self.on_clear_after_send_switch_toggled)
+
+    def on_clear_after_send_switch_toggled(self, *args):
+        state = self.clear_after_send_switch.props.state
+
+        if state:
+            self.settings.set_boolean("clear-after-send", True)
+        else:
+            self.settings.set_boolean("clear-after-send", False)
