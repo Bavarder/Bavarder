6ef07107fe83e85ef6959ec612c86e79e0dee12b
0xMRTT <0xMRTT@proton.me>
2023-05-01 01:17:40 +0200

    provider: add dialogpt

---
+++ src/provider/hfdialogpt.py
@@ -0,0 +1,44 @@
+from .huggingface import BaseHFProvider
+import json
+import socket
+import requests
+
+class HuggingFaceDialoGPTLargeProvider(BaseHFProvider):
+    name = "DialoGPT"
+    slug = "dialogpt"
+    model = "microsoft/DialoGPT-large"
+    authorization = False
+
+    def ask(self, prompt):
+        try:
+            payload = json.dumps({
+                "inputs": {
+                    #"past_user_inputs": ["Which movie is the best ?"],
+                    #"generated_responses": ["It's Die Hard for sure."],
+                    "text": prompt
+                },
+            })
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            if self.authorization:
+                headers["Authorization"] = f"Bearer {self.api_key}"
+            url = f"https://api-inference.huggingface.co/models/{self.model}"
+            print(url)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            print(response.json())
+            response = response.json()["generated_text"]
+
+        # except NoApikey:
+        #     self.no_api_key()
+        #     return ""
+        except KeyError:
+            pass
+        except socket.gaierror:
+            self.no_connection()
+            return ""
+        else:
+            self.hide_banner()
+            print(response)
+            self.update_response(response)
+            return response
