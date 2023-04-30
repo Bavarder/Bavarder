127350cd957c06fb6017dbf20b5655db712c139c
0xMRTT <0xMRTT@proton.me>
2023-05-01 00:30:43 +0200

    provider: add alpaca

---
+++ src/provider/alpacalora.py
@@ -0,0 +1,59 @@
+from .base import BavarderProvider
+
+import socket
+import requests
+
+from gi.repository import Gtk, Adw
+class AlpacaLoRAProvider(BavarderProvider):
+    name = "Alpaca-LoRA"
+    slug = "alpacalora"
+
+    def __init__(self, win, app, *args, **kwargs):
+        super().__init__(win, app, *args, **kwargs)
+
+
+    def ask(self, prompt):
+        try:
+            response = requests.post("https://tloen-alpaca-lora.hf.space/run/predict", json={
+                "data": [
+                    prompt,
+                    prompt,
+                    0.1,
+                    0.75,
+                    40,
+                    4,
+                    128,
+                ]
+            }).json()
+        except socket.gaierror:
+            self.no_connection()
+            return ""
+        else:
+            self.win.banner.set_revealed(False)
+            r = response["data"][0]
+            self.update_response(r)
+            return r
+
+    @property
+    def require_api_key(self):
+        return False
+
+    def preferences(self):
+        self.no_preferences()
+        
+    def about(self):
+        about = Adw.AboutWindow(
+            transient_for=self.props.active_window,
+            application_name="Alpaca-LoRA",
+            developer_name="Stanford Alpaca",
+            developers=["0xMRTT https://github.com/0xMRTT"],
+            license_type=Gtk.License.GPL_3_0,
+            version=version,
+            copyright="© 2023 0xMRTT",
+        )
+    
+    def save(self):
+        return {}
+
+    def load(self, data):
+        pass
