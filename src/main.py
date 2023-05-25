201ede414abbd8dfd6b9a6e17758f83cebb290ab
0xMRTT <0xMRTT@proton.me>
2023-05-25 21:15:08 +0200

    feat: add threading

--- src/main.py
+++ src/main.py
@@ -47,41 +47,14 @@ import tempfile
 import re
 import requests
 
+from .threading import KillableThread
+
+from enum import auto, IntEnum
+
 class Step(IntEnum):
     CONVERT_HTML = auto()
     LOAD_WEBVIEW = auto()
     RENDER = auto()
-
-class KillableThread(threading.Thread):
-    def __init__(self, *args, **keywords):
-        threading.Thread.__init__(self, *args, **keywords)
-        self.killed = False
-    
-    def start(self):
-        self.__run_backup = self.run
-        self.run = self.__run     
-        threading.Thread.start(self)
-    
-    def __run(self):
-        sys.settrace(self.globaltrace)
-        self.__run_backup()
-        self.run = self.__run_backup
-    
-    def globaltrace(self, frame, event, arg):
-        if event == 'call':
-            return self.localtrace
-        else:
-            return None
-    
-    def localtrace(self, frame, event, arg):
-        if self.killed:
-            if event == 'line':
-                raise SystemExit()
-        return self.localtrace
-    
-    def kill(self):
-        self.killed = True
-
 
 ANNOUCEMENT_URL = "https://bavarder.codeberg.page/annoucements.json"
 
